fc95bc5f07586b751a2b5a0fb54af5da662db50c
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2022-06-10 18:56:44 +0300

    1

--- host_discovery_SQLite/SQLite_server.py
+++ host_discovery_SQLite/SQLite_server.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
-from host_discovery_SQLite.extensions import db
+from extensions import db
+from db_apis import get_all_hosts, set_host
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hostDB.db'
@@ -9,14 +11,11 @@ app.app_context().push()
 
 db.create_all()
 
-from host_discovery_SQLite.db_apis import get_all_hosts, set_host
 
 @app.route('/hosts', methods=["GET", "PUT"])
 def hosts():
-
     if request.method == "GET":
         return get_all_hosts()
-
 
     elif request.method == "PUT":
         hostname = request.args.get("hostname")
@@ -28,5 +27,14 @@ def hosts():
         return {}, 204
 
 
-if __name__ == '__main__':
+def main():
     app.run(debug=True)
+
+
+if __name__ == '__main__':
+
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n\nExiting SQLite_server!")
+        exit()
